c79c5415fe3fe6797507eac3ea6f8f3afa7b44a1
Bryan Edward Fernandes <bryan.efernandes@gmail.com>
2022-05-04 16:01:43 -0300

    Requisitos 54 a 59

---
+++ src/components/DoneRecipesCard.js
@@ -0,0 +1,115 @@
+import copy from 'clipboard-copy';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
+import './DoneRecipesCard.css';
+
+function DoneRecipesCard({ data, index }) {
+  const { id, type, nationality, category, alcoholicOrNot,
+    name, image, doneDate, tags } = data;
+  const [isCopied, setIsCopied] = useState(false);
+  const history = useHistory();
+
+  const redirectOnClick = () => {
+    if (type === 'food') {
+      history.push(`/foods/${id}`);
+    } else {
+      history.push(`/drinks/${id}`);
+    }
+  };
+
+  return (
+    <div
+      className="DoneRecipesCard-Wrapper"
+    >
+      <button
+        className="DoneRecipesCard-Image"
+        type="button"
+        onClick={ redirectOnClick }
+      >
+        <img
+          data-testid={ `${index}-horizontal-image` }
+          src={ image }
+          alt={ name }
+        />
+      </button>
+      <div
+        className="DoneRecipesCard-Container"
+      >
+        <button
+          className="DoneRecipesCard-Info"
+          type="button"
+          onClick={ redirectOnClick }
+        >
+          <div
+            data-testid={ `${index}-horizontal-top-text` }
+            className="DoneRecipesCard-TopText"
+          >
+            {
+              type === 'food'
+                ? `${nationality} - ${category}`
+                : alcoholicOrNot
+            }
+          </div>
+          <div
+            data-testid={ `${index}-horizontal-name` }
+            className="DoneRecipesCard-Name"
+          >
+            {name}
+          </div>
+        </button>
+        <div
+          className="DoneRecipesCard-Icons"
+        >
+          <div
+            data-testid={ `${index}-horizontal-done-date` }
+          >
+            {`Done in: ${doneDate}`}
+          </div>
+          <button
+            type="button"
+            onClick={ () => {
+              setIsCopied(true);
+              if (type === 'food') {
+                copy(`http://localhost:3000/foods/${id}`);
+              }
+              copy(`http://localhost:3000/drinks/${id}`);
+            } }
+          >
+            <img
+              data-testid={ `${index}-horizontal-share-btn` }
+              src={ shareIcon }
+              alt="Share Icon"
+            />
+          </button>
+        </div>
+        <div
+          className="DoneRecipesCard-Tags"
+        >
+          {
+            tags.length > 0
+            && tags.map((tag) => (
+              <div
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+                key={ tag }
+              >
+                {tag}
+              </div>
+            ))
+          }
+        </div>
+        <div className="DoneRecipesCard-IsCopied">
+          {isCopied && 'Link copied!'}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+DoneRecipesCard.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any),
+  index: PropTypes.number,
+}.isRequired;
+
+export default DoneRecipesCard;
--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,13 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DoneRecipesCard from '../components/DoneRecipesCard';
 import Header from '../components/Header';
+import { readInLocalStorage, saveInLocalStorage } from '../services/localStorage';
 
 function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+  const [currentFilter, setCurrentFilter] = useState('');
+
+  useEffect(() => {
+    if (readInLocalStorage('doneRecipes') === null) {
+      saveInLocalStorage('doneRecipes', []);
+      setDoneRecipes(readInLocalStorage('doneRecipes'));
+    } else {
+      setDoneRecipes(readInLocalStorage('doneRecipes'));
+    }
+  }, []);
+
   return (
     <div>
       <Header
         title="Done Recipes"
       />
-      <p>RECEITAS FEITAS</p>
+      <section
+        className="FavoriteRecipesFilters"
+      >
+        <button
+          data-testid="filter-by-all-btn"
+          type="button"
+          onClick={ () => setCurrentFilter('') }
+        >
+          All
+        </button>
+        <button
+          data-testid="filter-by-food-btn"
+          type="button"
+          onClick={ () => setCurrentFilter('food') }
+        >
+          Food
+        </button>
+        <button
+          data-testid="filter-by-drink-btn"
+          type="button"
+          onClick={ () => setCurrentFilter('drink') }
+        >
+          Drinks
+        </button>
+      </section>
+      <section>
+        {
+          doneRecipes.length > 0
+            ? doneRecipes.filter(({ type }) => type.includes(currentFilter))
+              .map((data, index) => (
+                <DoneRecipesCard
+                  data={ data }
+                  index={ index }
+                  key={ data.id }
+                />
+              ))
+            : (<div>List is empty</div>)
+        }
+      </section>
     </div>
   );
 }
