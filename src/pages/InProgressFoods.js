d503e6f9b764d83bb3cef273a2810aa4749d2e9e
Johnata Barreto <johnatabarreto@hotmail.com>
2022-05-04 15:41:24 -0300

    item  47 iniciado

--- src/pages/InProgressFoods.js
+++ src/pages/InProgressFoods.js
@@ -1,9 +1,50 @@
 import React from 'react';
 
 function InProgressFoods() {
+  const inngredienteTest = `${index}-ingredient-step`;
+
   return (
     <div>
-      <p>PÁGINA DE COMIDAS EM PROCESSO</p>
+      <div
+        data-testid="recipe-photo"
+      >
+        Foto
+      </div>
+      <div
+        data-testid="recipe-title"
+      >
+        title
+      </div>
+      <div
+        data-testid="share-btn"
+      >
+        botão compartilhar
+      </div>
+      <div
+        data-testid="favorite-btn"
+      >
+        botão favoriar
+      </div>
+      <div
+        data-testid="recipe-category"
+      >
+        Categoria
+      </div>
+      <div
+        data-testid={ inngredienteTest }
+      >
+        ingredientes
+      </div>
+      <div
+        data-testid="instructions"
+      >
+        Instruções
+      </div>
+      <div
+        data-testid="finish-recipe-btn"
+      >
+        botão finalizar
+      </div>
     </div>
   );
 }
